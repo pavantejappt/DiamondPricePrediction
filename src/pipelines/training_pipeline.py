e85b4932835e1ee995c855211eaf91c91e47ec05
pavantejappt <pavantejappt082001@gmail.com>
2023-05-15 11:32:45 +0530

    final predction done

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -4,7 +4,7 @@ from src.logger import logging
 from src.exception import CustomException
 
 import pandas as pd
-
+from src.components.model_trainer import ModelTrainer
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 
@@ -14,3 +14,7 @@ if __name__=='__main__' :
     print(train_data_path,test_data_path)
     data_transformation=DataTransformation()
     train_arr,test_arr,obj_path=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
+
